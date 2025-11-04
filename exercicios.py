f2fc5cbaf69b8e555a3638850a6bc6df173fd2c0
donati <joao.donati@gmail.com>
2025-11-04 19:15:45 +0000

    commmit 02

---
+++ exercicios.py
@@ -0,0 +1,6 @@
+#Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+a=float(input( "Digite o primeiro número inteiro: "))
+b=float(input( "Digite o segundo número inteiro: "))
+
+print(a//b)
